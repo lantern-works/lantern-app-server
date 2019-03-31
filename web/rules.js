7ca48fd9e3a5dfae68cdfa4879176329a2f3705d
West Lane <west@paperequator.com>
2019-03-31 13:33:22 -0400

    Basic Ducktype Data Validation in Rules

--- web/rules.js
+++ web/rules.js
@@ -6,6 +6,43 @@ const namespace = '__LX__'
 const safeguard = ['pkg', 'org', 'ctx']
 
 /**
+* Marker Validation
+*/
+const validateMarker = (id, data) => {
+    if (!data.hasOwnProperty('o') || typeof(data['o']) !== 'string') {
+        log.warn(`[rules] missing or invalid owner for marker ${id}`)
+        log.warn(data)
+        return false
+    }
+    if (!data.hasOwnProperty('t') || typeof(data['t']) !== 'string' || data['t'][0] != '%') {
+        log.warn(`[rules] missing or invalid tags for marker ${id}`)
+        log.warn(data)
+        return false
+    }
+    return true
+}
+
+/**
+* Removes GunDB specific metadata when analyzing input
+*/
+const removeMetadata = (data) => {
+
+    let newData = data
+
+    if (typeof(data) == 'object') {
+
+        let meta = ['_', '#', '>']
+        newData = {}
+        Object.keys(data).forEach(k => {
+            if (meta.indexOf(k) == -1) {
+                newData[k] = data[k]
+            }
+        })
+    }
+    return newData
+}
+
+/**
 * Database update validator
 */
 const rules = (msg) => {
@@ -15,6 +52,21 @@ const rules = (msg) => {
     if (msg.put) {
         isValid = msg.headers && msg.headers.token
         let token = (isValid ? msg.headers.token : null)
+
+        // check for required parameters
+        Object.keys(msg.put).forEach(id => {
+            let item = removeMetadata(msg.put[id])
+
+            // ducktype markers and enforce rules
+            if (item.hasOwnProperty('g')) {
+                isValid = validateMarker(id, item)
+            }
+        })
+
+
+        if (!isValid) return false
+
+        // avoid a top-level namespace reset at any cost to avoid accidents
         if (msg.put.hasOwnProperty(namespace)) {
             let attempt = msg.put[namespace]
             safeguard.forEach(key => {
@@ -25,20 +77,23 @@ const rules = (msg) => {
                 } 
             })
         }
-         if (!msg.how || msg.how !== 'mem') {
+
+        // listen for messages trying to save to this server
+        if ( (!msg.how || msg.how !== 'mem') && msg.hasOwnProperty('><')) {
            log.debug(`--------- PUT ${isValid ? 'valid' : 'blocked'} ---------`)
            log.debug(`%% user token = ${token} %%`)
-            Object.keys(msg.put).forEach(k => {
+           Object.keys(msg.put).forEach(k => {
                 if (k[0] == '~') return // ignore user-specific puts
                 Object.keys(msg.put[k]).forEach(field => {
                     if (field !== '#' && field !== '>'&& field !== '_') {
                         log.debug(`  ${k} ${field} = ${JSON.stringify(msg.put[k][field])}`)
                     }
                 })
-            })       
-           log.debug(`--------- /PUT ${isValid ? 'valid' : 'blocked'} ---------`)
+           })
+           log.debug(`--------- /PUT ${isValid ? 'valid' : 'blocked'} ---------/n`)
         }
     }
+
     return isValid
 }
 
