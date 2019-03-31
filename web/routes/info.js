ada1dd16795e0d91b0696c1c56e056b66bd2e97b
West Lane <west@paperequator.com>
2019-03-31 13:33:06 -0400

    Include Rules Hash in API Info GET

--- web/routes/info.js
+++ web/routes/info.js
@@ -2,6 +2,10 @@
 const headers = require('../middleware/headers')
 module.exports = (serv) => {
     serv.get('/api/info', headers, (req, res) => {
-        return res.status(200).json({"online": res.app.locals.online, "cloud": res.app.locals.cloud})
+        return res.status(200).json({
+            "online": res.app.locals.online, 
+            "cloud": res.app.locals.cloud,
+            "rules": res.app.locals.rules
+        })
      })
 }
