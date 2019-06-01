cf82abb15e3235055c68858ae95bbd76fad69590
West Lane <west@paperequator.com>
2019-06-01 13:27:09 -0400

    Make Prime Number Available to Client

--- web/routes/info.js
+++ web/routes/info.js
@@ -4,6 +4,7 @@ module.exports = (serv) => {
     serv.get('/api/info', headers, (req, res) => {
         return res.status(200).json({
             'peer': res.app.locals.peer,
+            'prime': res.app.locals.prime,
             'online': res.app.locals.online,
             'cloud': res.app.locals.cloud,
             'rules': res.app.locals.rules
